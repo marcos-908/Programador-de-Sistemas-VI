3e4826ac030e96e557590de84925de9a62900de9
marcos-908 <marcosprogramador908@gmail.com>
2026-06-03 17:01:24 -0300

    03/06/2026

---
+++ media.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    double N1 = 5.0;
+	double N2 = 7.1;
+	double P1 = 3.5;
+	double P2 = 7.5;
+	double MEDIA = 0;
+	
+	scanf("%lf", &N1);
+	scanf("lf", &N2);
+	
+	MEDIA=(N1*P1)+(N2*P2)/11;
+ 
+    return 0;
+}
